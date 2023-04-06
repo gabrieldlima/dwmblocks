39fc30dd9433f0e2923bb70ebb162ef2df113c61
Gabriel Lima <gabrielgsdlima@gmail.com>
2023-04-06 06:28:42 -0300

    Merge scripts from my dotfiles repo

--- blocks.def.h
+++ blocks.def.h
@@ -1,11 +1,13 @@
+#define PATH(name) "/home/gabriel/.local/src/dwmblocks/scripts/"name
+
 // Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
     /*Icon*/ /*Command*/    /*Update Interval*/ /*Update Signal*/
-    {" ",    "sb-kernel",   600,                 0},
-    {"",     "sb-cpu",      1,                   0},
-    {"",     "sb-memory",   1,                   0},
-    {"",     "sb-date",     60,                  0},
-    {"",     "sb-clock",    60,                  0},
+    {" ",    PATH("kernel"),   600,                 0},
+    {"",     PATH("cpu"),      1,                   0},
+    {"",     PATH("memory"),   1,                   0},
+    {"",     PATH("date"),     60,                  0},
+    {"",     PATH("clock"),    60,                  0},
 };
 
 // Sets delimeter between status commands. NULL character ('\0') means no delimeter.
